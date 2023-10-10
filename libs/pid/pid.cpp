adf137c46a6979e452698b08602a7a30dd63dcea
patrikpordi <ppordi@umd.edu>
2023-10-10 12:57:06 -0400

    cpplint and cppcheck

--- libs/pid/pid.cpp
+++ libs/pid/pid.cpp
@@ -1,88 +1,72 @@
 /**
  * @file pid.cpp
  * @author Patrik Dominik Pördi (ppordi@umd.edu)
- * @brief 
+ * @brief
  * @version 0.2
  * @date 2023-10-10
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
- 
+
 #include "pid.hpp"
 
 // Constructor to initialize Kp, Ki, and Kd values to 1.0 and dt to 0.01
 /** @brief Class for PID controller
-    * 
-    */
-pid_controller::pid_controller()
-{
-    Kp = 1.0;
-    Ki = 1.0;
-    Kd = 1.0;
-    dt = 0.01;
-    
+ *
+ */
+pid_controller::pid_controller() {
+  Kp = 1.0;
+  Ki = 1.0;
+  Kd = 1.0;
+  dt = 0.01;
 }
 
-// Compute function to calculate the PID control output based on the input signal
+// Compute function to calculate the PID control output based on the input
+// signal
 /**
- * @brief 
- * 
- * @param input_signal 
- * @param ref_signal 
- * @return float 
+ * @brief
+ *
+ * @param input_signal
+ * @param ref_signal
+ * @return float
  */
-float pid_controller::compute(float input_signal,float ref_signal)
-{
-    // Implement PID control logic
-    float output_vel;
-    float integral=0;
-    float error_i=0;
-    float error = input_signal - ref_signal;
+float pid_controller::compute(float input_signal, float ref_signal) {
+  // Implement PID control logic
+  float output_vel;
+  float integral = 0;
+  float error_i = 0;
+  float error = input_signal - ref_signal;
 
-    // Proportional term
-    float proportional = error;
+  // Integral term
+  integral += (error * dt);
 
-    // Integral term
-    integral += (error*dt);
+  // Derivative term
+  float derivative = (error - error_i) / dt;
 
+  // PID calculation
+  output_vel = ref_signal + Kp * error + Ki * integral + Kd * derivative;
 
-    // Derivative term
-    float derivative = (error - error_i)/dt;
-
-    // PID calculation
-    output_vel= ref_signal + Kp * error + Ki * integral + Kd * derivative;
-
-
-    return output_vel;
+  return output_vel;
 }
 
 // Functions to set the values of Kp, Ki, and Kd individually
 /**
- * @brief 
- * 
- * @param p 
+ * @brief
+ *
+ * @param p
  */
-void pid_controller::set_Kp(float p)
-{
-    Kp = p;
-}
+void pid_controller::set_Kp(float p) { Kp = p; }
 
 /**
- * @brief 
- * 
- * @param i 
+ * @brief
+ *
+ * @param i
  */
-void pid_controller::set_Ki(float i)
-{
-    Ki = i;
-}
+void pid_controller::set_Ki(float i) { Ki = i; }
 /**
- * @brief 
- * 
- * @param d 
+ * @brief
+ *
+ * @param d
  */
-void pid_controller::set_Kd(float d)
-{
-    Kd = d;
-}
+void pid_controller::set_Kd(float d) { Kd = d; }
--- test/test.cpp
+++ test/test.cpp
@@ -1,25 +1,25 @@
 /**
  * @file test.cpp
  * @author Patrik Dominik Pördi (ppordi@umd.edu)
- * @brief 
+ * @brief
  * @version 0.2
  * @date 2023-10-10
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
-#include "pid.hpp"
 #include <gtest/gtest.h>
 
+#include "../libs/pid/pid.hpp"
 
 /**
  * @brief Construct a new TEST object
- * 
+ *
  */
 TEST(pid_controller, Test_Case_1) {
   // Initialize a PID controller with Kp, Ki, and Kd
   pid_controller controller;
-  controller.set_Kp( 0.001);
+  controller.set_Kp(0.001);
   controller.set_Ki(0.0);
   controller.set_Kd(0.01);
 
@@ -28,17 +28,17 @@ TEST(pid_controller, Test_Case_1) {
   float target_velocity = 11;
 
   // Calculate the PID control output
-  float output = controller.compute(target_velocity,start_velocity);
-  
-  // Check if the calculated output matches the expected output within a tolerance
-  EXPECT_NEAR(output, 11, 0.05); // Adjust tolerance as needed
-  
+  float output = controller.compute(target_velocity, start_velocity);
+
+  // Check if the calculated output matches the expected output within a
+  // tolerance
+  EXPECT_NEAR(output, 11, 0.05);  // Adjust tolerance as needed
 }
 
 TEST(pid_controller, Test_Case_2) {
   // Initialize a PID controller with Kp, Ki, and Kd
   pid_controller controller;
-  controller.set_Kp( 0.1);
+  controller.set_Kp(0.1);
   controller.set_Ki(0.0);
   controller.set_Kd(0.1);
 
@@ -47,8 +47,9 @@ TEST(pid_controller, Test_Case_2) {
   float target_velocity = 8;
 
   // Calculate the PID control output
-  float output = controller.compute(target_velocity,start_velocity);
-  std::cout<<output<<std::endl;
-  // Check if the calculated output matches the expected output within a tolerance
-  EXPECT_NEAR(output, 8, 0.2); // Adjust tolerance as needed
+  float output = controller.compute(target_velocity, start_velocity);
+  std::cout << output << std::endl;
+  // Check if the calculated output matches the expected output within a
+  // tolerance
+  EXPECT_NEAR(output, 8, 0.2);  // Adjust tolerance as needed
 }
